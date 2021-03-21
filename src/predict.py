1b2bd358f37689ad2fd36c61db5636d503d460d7
Andrew Pope <andrew@andrewapope.com>
2021-03-21 14:09:21 -0600

    sagemaker updates

--- src/predict.py
+++ src/predict.py
@@ -13,7 +13,7 @@ import torch.utils.data
 
 from model import LSTMClassifier
 
-from utils import review_to_words, convert_and_pad, load_model
+from utils import encode_single_input, model_fn
 
 
 def input_fn(serialized_input_data, content_type):
@@ -34,30 +34,13 @@ def predict_fn(input_data, model):
     
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
-    
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-    
-    review_words = review_to_words(input_data)
-    data_X, data_len = convert_and_pad(model.word_dict, review_words)    
-    
-    # Using data_X and data_len we construct an appropriate input tensor. Remember
-    # that our model expects input data of the form 'len, review[500]'.
-    
-    data_pack = np.hstack((data_len, data_X))
-    data_pack = data_pack.reshape(1, -1)
-    
-    data = torch.from_numpy(data_pack)
-    data = data.to(device)
+
+    data = encode_single_input(input_data, model.word_dict)
+    data = torch.from_numpy(data).to(device)
 
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
-    
     y_hat = model(data).detach().cpu().numpy()
     result = np.round(y_hat).astype(np.int)
 
