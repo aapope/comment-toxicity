a195b13cc69ec2b3087d1d6304d02705a69591ff
Andrew Pope <andrew@andrewapope.com>
2021-03-21 15:39:01 -0600

    adds validation metrics logging

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import json
 import os
 import pickle
@@ -8,6 +9,12 @@ import pandas as pd
 import torch
 import torch.optim as optim
 import torch.utils.data
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    roc_auc_score
+)
 
 from model import LSTMClassifier
 
@@ -46,20 +53,29 @@ def model_fn(model_dir):
     return model
 
 
-def _get_train_data_loader(batch_size, training_dir):
-    print("Get train data loader.")
+def _get_data_loader(batch_size, data_dir, is_training=True):
+    if is_training:
+        print("Get train data loader.")
+    else:
+        print('Get validation/test data loader')
 
-    train_data = joblib.load(os.path.join(training_dir, 'data.joblib'))
+    train_data = joblib.load(os.path.join(data_dir, 'data.joblib'))
 
     # first 6 are the targets
-    train_y = torch.from_numpy(train_data[:, :6]).float()
-    train_X = torch.from_numpy(train_data[:, 6:]).long()
-
-    print(f'{train_y.shape}, {train_X.shape}')
-
+    train_y = torch.from_numpy(train_data[:250, :6]).float()
+    train_X = torch.from_numpy(train_data[:250, 6:]).long()
     train_ds = torch.utils.data.TensorDataset(train_X, train_y)
 
-    return torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
+    if is_training:
+        print('Upsampling records with any positive labels...')
+        # upsample examples where any label=1. should help the class imbalance...
+        any_label = train_data[:250, :6].max(axis=1)
+        class_weights = [any_label.mean(), 1 - any_label.mean()]
+        weights = [class_weights[any_label[i]] for i in range(any_label.shape[0])]
+        sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
+        return torch.utils.data.DataLoader(train_ds, batch_size=batch_size, sampler=sampler)
+    else:
+        return train_X, train_y
 
 
 def train(model, train_loader, epochs, optimizer, loss_fn, device):
@@ -95,6 +111,32 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
         print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
+def log_eval_metrics(model, val_X, val_y, device):
+    val_X = val_X.to(device)
+    predict_y = model(val_X).cpu().detach().numpy()
+
+    metrics = defaultdict(list)
+    for label in range(6):
+        print(label)
+        val_y_class = val_y[:, label]
+        predict_y_class = predict_y[:, label]
+        predict_y_class_label = predict_y_class.round()
+
+        metrics['accuracy'].append(accuracy_score(val_y_class, predict_y_class_label))
+        metrics['precision'].append(precision_score(val_y_class, predict_y_class_label))
+        metrics['recall'].append(recall_score(val_y_class, predict_y_class_label))
+        metrics['roc_auc'].append(roc_auc_score(val_y_class, predict_y_class))
+
+    avg_metrics = {}
+    for name, results in metrics.items():
+        print(f'{name} at class level: {results}')
+        avg_metrics[name] = sum(results) / len(results)
+
+    print(*[f'{name}: {result};' for name, result in avg_metrics.items()])
+    
+    
+    
+
 if __name__ == '__main__':
     # All of the model parameters and training parameters are sent as arguments when the script
     # is executed. Here we set up an argument parser to easily access the parameters.
@@ -123,26 +165,27 @@ if __name__ == '__main__':
     parser.add_argument('--hosts', type=list, default=json.loads(os.getenv('SM_HOSTS', '[]')))
     parser.add_argument('--current-host', type=str, default=os.getenv('SM_CURRENT_HOST', ''))
     parser.add_argument('--model-dir', type=str, default=os.getenv('SM_MODEL_DIR', 'model/'))
-    parser.add_argument('--data-dir', type=str, default=os.getenv('SM_CHANNEL_TRAINING', 'processed/train/'))
+    parser.add_argument('--train-data-dir', type=str, default=os.getenv('SM_CHANNEL_TRAINING', 'processed/train/'))
+    parser.add_argument('--val-data-dir', type=str, default=os.getenv('SM_CHANNEL_EVAL', 'processed/val/'))
     parser.add_argument('--num-gpus', type=int, default=os.getenv('SM_NUM_GPUS', '0'))
 
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = 'cpu'
     print("Using device {}.".format(device))
 
     torch.manual_seed(args.seed)
 
     # Load the training data.
-    train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
+    train_loader = _get_data_loader(args.batch_size, args.train_data_dir, is_training=True)
+    val_X, val_y = _get_data_loader(args.batch_size, args.val_data_dir, is_training=False)
 
     # Build the model.
     model = LSTMClassifier(args.embedding_dim, args.num_lstm_layers, args.hidden_dims,
                            args.vocab_size).to(device)
     print(model)
 
-    with open(os.path.join(args.data_dir, "vocab.joblib"), "rb") as f:
+    with open(os.path.join(args.train_data_dir, "vocab.joblib"), "rb") as f:
         model.word_dict = joblib.load(f)
 
     print("Model loaded with embedding_dim {}, lstm_layers {}, hidden_dims {}, vocab_size {}.".format(
@@ -154,6 +197,9 @@ if __name__ == '__main__':
     loss_fn = torch.nn.BCELoss()
 
     train(model, train_loader, args.epochs, optimizer, loss_fn, device)
+    log_eval_metrics(model, val_X, val_y, device)
+
+    os.makedirs(args.model_dir, exist_ok=True)
 
     # Save the parameters used to construct the model
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
