d85847d8c1fa7fe6b3d59ef6dd8a0b737cefc8db
Andrew Pope <andrew@andrewapope.com>
2021-03-21 14:17:27 -0600

    fixes circular references

--- src/utils.py
+++ src/utils.py
@@ -124,35 +124,3 @@ def encode_text(df, use_cache=True, vocab_length=10000, max_length=500):
 
 #=====================
 
-def model_fn(model_dir):
-    # First, load the parameters used to create the model.
-    model_info = {}
-    model_info_path = os.path.join(model_dir, 'model_info.pth')
-    with open(model_info_path, 'rb') as f:
-        model_info = torch.load(f)
-
-    print("model_info: {}".format(model_info))
-
-    # Determine the device and construct the model.
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LSTMClassifier(
-        model_info['embedding_dim'],
-        model_info['num_lstm_layers'],
-        model_info['hidden_dims'],
-        model_info['vocab_size']
-    )
-
-    # Load the stored model parameters.
-    model_path = os.path.join(model_dir, 'model.pth')
-    with open(model_path, 'rb') as f:
-        model.load_state_dict(torch.load(f))
-
-    # Load the saved word_dict.
-    word_dict_path = os.path.join(model_dir, 'vocab.joblib')
-    with open(word_dict_path, 'rb') as f:
-        model.word_dict = joblib.load(f)
-
-    model.to(device).eval()
-
-    print("Done loading model.")
-    return model
